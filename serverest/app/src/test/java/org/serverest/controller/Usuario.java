4e54b2664ebc52bc2715ead3f7e6c5a3e1103e5c
Davi Hardman <dghardman@gmail.com>
2025-02-04 00:12:31 -0300

    Updating initial code

---
+++ serverest/app/src/test/java/org/serverest/controller/Usuario.java
@@ -0,0 +1,43 @@
+package org.serverest.controller;
+
+import org.serverest.model.UsuarioDTO;
+import io.restassured.http.ContentType;
+import org.serverest.util.Ambiente;
+import org.serverest.util.Endpoint;
+import static org.hamcrest.CoreMatchers.is;
+import static io.restassured.RestAssured.given;
+public class Usuario {
+    public static String cadastrar(UsuarioDTO usuarioDTO, Integer statusCode, String mensagem) {
+
+        return given()
+                .body("{\n" +
+                        "  \"nome\": \"" + usuarioDTO.getNome() + "\",\n" +
+                        "  \"email\": \"" + usuarioDTO.getEmail() + "\",\n" +
+                        "  \"password\": \"" + usuarioDTO.getPassword() + "\",\n" +
+                        "  \"administrador\": \"" + usuarioDTO.getAdministrador() + "\"\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+            .when()
+                .post(Ambiente.localhost + Endpoint.usuarios)
+            .then()
+                .statusCode(statusCode)
+                .body("message", is(mensagem))
+                .extract().path("_id");
+    }
+
+    public static String autenticar(UsuarioDTO usuarioDTO, Integer statusCode, String message) {
+
+        return given()
+                .body("{\n" +
+                        "  \"email\": \"" + usuarioDTO.getEmail() + "\",\n" +
+                        "  \"password\": \"" + usuarioDTO.getPassword() + "\"\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+            .when()
+                .post(Ambiente.localhost + Endpoint.login)
+            .then()
+                .statusCode(statusCode)
+                .body("message", is(message))
+                .extract().path("authorization");
+    }
+}
---
+++ serverest/app/src/test/java/org/serverest/util/Mensagem.java
@@ -0,0 +1,9 @@
+package org.serverest.util;
+
+public class Mensagem {
+
+    public static final String cadastroSucesso = "Cadastro realizado com sucesso";
+    public static final String loginSucesso = "Login realizado com sucesso";
+    public static final String emailJaUsado = "Este email já está sendo usado";
+    public static final String cancelarCompra = "Registro excluído com sucesso. Estoque dos produtos reabastecido";
+}
