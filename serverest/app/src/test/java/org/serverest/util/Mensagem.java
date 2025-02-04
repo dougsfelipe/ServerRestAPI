76028e4f24a848cf6e3f5b3557c3984dbf9c8095
Davi Hardman <dghardman@gmail.com>
2025-02-04 00:34:49 -0300

    Changing messagem class encoding

--- serverest/app/src/test/java/org/serverest/util/Mensagem.java
+++ serverest/app/src/test/java/org/serverest/util/Mensagem.java
@@ -4,6 +4,6 @@ public class Mensagem {
 
     public static final String cadastroSucesso = "Cadastro realizado com sucesso";
     public static final String loginSucesso = "Login realizado com sucesso";
-    public static final String emailJaUsado = "Este email j· est· sendo usado";
-    public static final String cancelarCompra = "Registro excluÌdo com sucesso. Estoque dos produtos reabastecido";
+    public static final String emailJaUsado = "Este email j√° est√° sendo usado";
+    public static final String cancelarCompra = "Registro exclu√≠do com sucesso. Estoque dos produtos reabastecido";
 }
