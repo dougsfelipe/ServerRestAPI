8f9a1faa1b3e7c7df2cfd21b4270bcfd0476d9c2
Davi Hardman <dghardman@gmail.com>
2025-02-04 18:50:16 -0300

    Removing product functions

--- serverest/app/src/test/java/org/serverest/controller/Produto.java
+++ serverest/app/src/test/java/org/serverest/controller/Produto.java
@@ -7,32 +7,5 @@ import org.serverest.util.Endpoint;
 import static org.hamcrest.CoreMatchers.is;
 import static io.restassured.RestAssured.given;
 public class Produto {
-    public static String cadastrar(ProdutoDTO produtoDTO, String usuarioToken, Integer statusCode, String mensagem) {
 
-        return given()
-                .header("Authorization", usuarioToken)
-                .body("{\n" +
-                        "  \"nome\": \"" + produtoDTO.getNome() + "\",\n" +
-                        "  \"preco\": " + produtoDTO.getPreco() + ",\n" +
-                        "  \"descricao\": \"" + produtoDTO.getDescricao() + "\",\n" +
-                        "  \"quantidade\": " + produtoDTO.getQuantidade() + "\n" +
-                        "}")
-                .contentType(ContentType.JSON)
-                .when()
-                .post(Ambiente.localhost + Endpoint.produtos)
-                .then()
-                .statusCode(statusCode)
-                .body("message", is(mensagem))
-                .extract().path("_id");
-    }
-
-    public static void verificarEstoque(String id, Integer quantidadeEsperada, Integer statusCode) {
-
-        given()
-                .pathParam("_id", id)
-                .when()
-                .get(Ambiente.localhost + Endpoint.produtosId)
-                .then()
-                .body("quantidade", is(quantidadeEsperada));
-    }
 }
