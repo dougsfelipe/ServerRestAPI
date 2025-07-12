af5dcd3e6da9295508fc666e0c064fcae262b6e5
Davi Hardman <dghardman@gmail.com>
2025-07-12 16:29:57 -0300

    Adjusting product factory

--- serverest/app/src/test/java/org/serverest/factory/ProdutoFactory.java
+++ serverest/app/src/test/java/org/serverest/factory/ProdutoFactory.java
@@ -5,12 +5,23 @@ import org.serverest.model.ProdutoDTO;
 
 public class ProdutoFactory {
 
-    public static ProdutoDTO criarComQuantidadeInicial(int quantidadeInicial) {
+    public static ProdutoDTO criarLivro(Integer preco, Integer quantidadeInicial) {
+        ProdutoDTO produtoDTO = new ProdutoDTO();
+        Faker faker = new Faker();
+        String nome = faker.book().title();
+        produtoDTO.setNome(nome);
+        produtoDTO.setPreco(preco);
+        produtoDTO.setDescricao("Livro");
+        produtoDTO.setQuantidade(quantidadeInicial);
+        return produtoDTO;
+    }
+
+    public static ProdutoDTO criarProduto(Integer preco, Integer quantidadeInicial) {
         ProdutoDTO produtoDTO = new ProdutoDTO();
         Faker faker = new Faker();
         String nome = faker.commerce().productName();
         produtoDTO.setNome(nome);
-        produtoDTO.setPreco(10);
+        produtoDTO.setPreco(preco);
         produtoDTO.setDescricao("Produto");
         produtoDTO.setQuantidade(quantidadeInicial);
         return produtoDTO;
